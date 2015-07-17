c1e980905e8b320b3f9b08476adfec3609882628
Greg <greg@example.com>
2015-07-17 08:03:57 +0200

    Town model

--- application/models/town_model.php
+++ application/models/town_model.php
@@ -17,9 +17,19 @@ class Town_model extends CI_Model
     
     
     public function GetRow($keyword) {        
+      /*
         $this->db->order_by('id', 'DESC');
         $this->db->like("miasto", $keyword);
         return $this->db->get('tbl_miasta')->result_array();
+      */
+        $this->db->select('tbl_miasta.id, miasto,wojewodztwo');
+        $this->db->from('tbl_miasta');
+        $this->db->join('tbl_wojewodztwa', 'tbl_miasta.id_woj = tbl_wojewodztwa.id');
+        $this->db->order_by('tbl_miasta.id', 'DESC');
+        $this->db->like("miasto", $keyword);
+        return $this->db->get()->result_array();
+
+        
     }
 
     
